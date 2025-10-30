e59ef93169912a70d1b162a5c0dd4347a6926a0f
Derin-WJ <derinsoysalis2009@gmail.com>
2025-10-30 15:04:33 +0000

    1st day

--- PatternGenerator.java
+++ PatternGenerator.java
@@ -1,17 +1,36 @@
+//Derin Soysal 
+//Period 4
+//10/30/25
+//This program
 public class PatternGenerator{
-    public static void main(String [args]){
+    public static void main(String[] args){
+
+        
 
 
 
 
 
+    }
 
-
-
-
-
-
-
+    public static void stars(int rows){
+        int i = 0;
+        String stars = "";
+        
+        
+            if (rows == 1){
+            System.out.println("*");
+            } else if (rows > 1) {
+                do {
+                    System.out.println("*");
+                } while(rows == 1);
+                    while (rows > 1 && rows <= i){
+                        System.out.println("**");
+                        i++;
+                    }
+                
+            } 
+        System.out.println(stars(3));
 
 
 
