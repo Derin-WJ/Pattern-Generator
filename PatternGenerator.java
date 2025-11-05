d895db969b455a76ef89603ad3e0ba22e6ac3d9d
Derin-WJ <derinsoysalis2009@gmail.com>
2025-11-05 16:32:14 +0000

    2nd day

--- PatternGenerator.java
+++ PatternGenerator.java
@@ -5,34 +5,52 @@
 public class PatternGenerator{
     public static void main(String[] args){
 
-        
-
-
-
-
+    stars(3);
+    triangle(11);
 
     }
 
     public static void stars(int rows){
         int i = 0;
-        String stars = "";
+        String temp = "*";
         
+         
         
-            if (rows == 1){
-            System.out.println("*");
-            } else if (rows > 1) {
-                do {
-                    System.out.println("*");
-                } while(rows == 1);
-                    while (rows > 1 && rows <= i){
-                        System.out.println("**");
-                        i++;
-                    }
-                
-            } 
-        System.out.println(stars(3));
-
-
-
+           while (i < rows){
+            System.out.println(temp);
+            temp += "**";
+            i++;
+           }
+        
     }
+
+
+    public static void triangle(int rows){
+        int i = 1;
+        String temp = "";
+        int j = 0;
+
+        if (rows == 1){
+            System.out.println(1);
+        } else {
+            System.out.println(1);
+            while (i < rows){
+                i++;
+                temp += i;
+                while(j < i){
+                    System.out.print(temp);
+                    j++;
+                }
+            j = 0;
+            temp = "";
+            System.out.println();
+            }
+        }
+    }
+
+    public static void odds(int start){}
+
+
+
+
 }
